c9c5c08c37a0a0e5de34d6814ece08b42cf4d1f7
Chih Hung Chen <hong19@chihhung-MacBook.local>
2017-12-13 10:53:48 +0800

    refactor(app): add comments

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
     .then(function (_client) {
       var client = _client;
 
+      // initialize file-explorer
       var explorer = window.Kloudless.explorer({
         // Explorer Initialization options here.
         app_id: "szWG8Xbec3v7Y_CRPKuPz_0ltKt0Ra991ZaDaqxVgrgtA0nB",
@@ -14,14 +15,16 @@ $(document).ready(function () {
       explorer.on('success', function (files) {
         // files is an array of JS objects that contain file metadata.
         console.log('Successfully selected files: ', files);
+        // only single selection
         var fileLink = files[0].link;
-        console.log('file link:', fileLink);
+        // insert the link to ticket editor
         client.interface.trigger('insertElement', {
           id: 'ticketContent',
           content: '<div><a href="' + fileLink + '">Append File</a></div>'
         })
       });
 
+      // bind a button to launch file-explorer 
       explorer.choosify($("#choose-file-btn"));
       // launch chooser
       explorer.choose();
